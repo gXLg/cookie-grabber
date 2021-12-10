e200e1cd48538d00ec8c1a4f9dd94ad3c0c1dc2c
gXLg <natgaev@gmail.com>
2021-12-10 18:23:19 +0100

    upd

--- cookie.py
+++ cookie.py
@@ -5,7 +5,7 @@ cookie = argv[2]
 
 con = sql.connect("/data/data/com.android.chrome/app_chrome/Default/Cookies")
 cur = con.cursor()
-for entry in cur.execute("SELECT * FROM Cookies WHERE name = '" + cookie + "' AND host_key LIKE '%" + sitename + "%'"):
-  print(entry[4])
+for entry in cur.execute("SELECT value FROM Cookies WHERE name = '" + cookie + "' AND host_key LIKE '%" + sitename + "%'"):
+  print(entry[0])
 
 con.close()
